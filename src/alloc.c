5bd0cb873b2e40c096b3418e68452e6424049270
A1phaZer0 <ini.universe@gmail.com>
2017-06-06 01:07:19 -0700

    one for all

--- src/alloc.c
+++ src/alloc.c
@@ -27,14 +27,44 @@ static struct block *freeblocks;
 void *allocate(unsigned long nbytes, unsigned a)
 {
 	struct block *ap;
+	unsigned mem;
 	
 	ap = arena[a];
 	
 	nbytes = roundup(nbytes, sizeof(union align));
 
-	if (ap->avail + nbytes > ap->limit) {
+	while (ap->avail + nbytes > ap->limit) {
 		/* get a new block */
+		if ((ap->next = freeblocks) != NULL) {
+			freeblocks = freeblocks->next;
+			ap = ap->next;
+		} else {
+			mem = sizeof(union header) + nbytes + 10 * 1024;
+			ap->next = (struct block *)malloc(mem);
+			ap = ap->next;
+			if (ap == NULL) {
+				printf("Insufficient memory!\n");
+				exit(1);
+			}
+			ap->limit = (char *)ap + mem;
+		}
+		ap->avail = (char*)((union header *)ap + 1);
+		ap->next = NULL;
+		arena[a] = ap;
 	}
 	ap->avail += nbytes;
 	return ap->avail - nbytes;
+}
+
+void *newarray(unsigned long m, unsigned long n, unsigned a)
+{
+	return allocate(m * n, a);
+}
+
+void deallocate(unsigned a)
+{
+	arena[a]->next = freeblocks;
+	freeblocks = first[a].next;
+	first[a].next = NULL;
+	arena[a] = &first[a];
 }
